ae62dd0ad942c8d7203beb86a29f466843330364
foxkids <foxkids@cartoon.local>
2025-06-16 16:59:45 +0300

    + wordpress/sql

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,41 @@ resource "docker_container" "nginx" {
     internal = 80
   }
 }
+
+resource "docker_image" "mysql" {
+  name = var.mysql
+  keep_locally = true
+}
+
+resource "docker_container" "mysql" {
+  name = "mysql-v9.3.0"
+  image = docker_image.mysql.image_id
+  env = [
+    "MYSQL_ROOT_PASSWORD=password",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=admin",
+    "MYSQL_PASSWORD=password"
+  ]
+}
+
+resource "docker_image" "wordpress" {
+  name = var.wordpress
+  keep_locally = true
+}
+
+resource "docker_container" "wordpress" {
+  name = "wordpress-v6.8.1"
+  image = docker_image.wordpress.image_id
+  ports {
+    external = 8899
+    internal = 80
+  }
+  env = [
+ #   "WORDPRESS_DB_HOST=mysql:3306",
+ #   "WORDPRESS_DB_USER=admin",
+ #   "WORDPRESS_DB_PASSWORD=password",
+ #   "WORDPRESS_DB_NAME=wordpress"
+     "ALLOW_EMPTY_PASSWORD=yes"
+  ]
+  depends_on = [docker_container.mysql]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "nginx" {
   type = string
   default = "nginx:1.27.5"
 }
+
+variable "mysql" {
+  type = string
+  default = "bitnami/mysql:9.3.0"
+}
+
+variable "wordpress" {
+  type = string
+  default = "bitnami/wordpress:6.8.1"
+}
