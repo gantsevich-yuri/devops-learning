5b36c78bcc335e3e6348923b3d1ec5f44f99dad1
foxkids <foxkids@cartoon.local>
2025-08-26 15:12:38 +0300

    zabbix task 2

---
+++ monitoring/zabbix-2/terraform/firewall.tf
@@ -0,0 +1,34 @@
+resource "yandex_vpc_security_group" "WAN" {
+  name       = "wan"
+  network_id = yandex_vpc_network.devnet.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "zabbix"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 10050
+    to_port        = 10051
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "web"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "from vm"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
