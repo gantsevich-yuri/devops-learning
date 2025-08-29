0f8dab66dc1c8a64bafe7aeaf40c8aec15eb0943
foxkids <foxkids@cartoon.local>
2025-08-29 17:30:28 +0300

    keepalived

---
+++ keepalived/terraform/compute.tf
@@ -0,0 +1,75 @@
+# Get information about existing Compute Image
+data "yandex_compute_image" "dev_image" {
+  family = "ubuntu-2204-lts"
+}
+
+# VM 1 (Zabbix Server and Zabbix client1)
+resource "yandex_compute_instance" "vm1" {
+  name        = "vm1"
+  hostname    = "vm1"
+  platform_id = "standard-v3"
+  zone        = var.zone_id
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.dev_image.id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.devsubnet_1.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.WAN.id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = "1"
+  }
+
+  scheduling_policy { preemptible = true }
+}
+
+# VM2 Zabbix client2
+resource "yandex_compute_instance" "vm2" {
+  name        = "vm2"
+  hostname    = "vm2"
+  platform_id = "standard-v3"
+  zone        = var.zone_id
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.dev_image.id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.devsubnet_1.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.WAN.id]
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = "1"
+  }
+
+  scheduling_policy { preemptible = true }
+}
+
---
+++ keepalived/terraform/network.tf
@@ -0,0 +1,37 @@
+# Create VPC Network
+resource "yandex_vpc_network" "devnet" {
+  name = "devnet"
+}
+
+# Create VPC Subnet 1
+resource "yandex_vpc_subnet" "devsubnet_1" {
+  v4_cidr_blocks = ["10.10.0.0/24"]
+  zone           = var.zone_id
+  network_id     = yandex_vpc_network.devnet.id
+  route_table_id = yandex_vpc_route_table.devroute.id
+}
+
+# Create VPC Subnet 2
+resource "yandex_vpc_subnet" "devsubnet_2" {
+  v4_cidr_blocks = ["10.10.1.0/24"]
+  zone           = var.zone_id
+  network_id     = yandex_vpc_network.devnet.id
+  route_table_id = yandex_vpc_route_table.devroute.id
+}
+
+# Create VPC Route Table
+resource "yandex_vpc_route_table" "devroute" {
+  name       = "devroute"
+  network_id = yandex_vpc_network.devnet.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.devnet_natgw.id
+  }
+}
+
+# Create VPC NAT Gateway
+resource "yandex_vpc_gateway" "devnet_natgw" {
+  name = "devnet-natgw"
+  shared_egress_gateway {}
+}
